b8f3ca1d46c11ae9c26279ed224405e4702b3832
Gabriella Koos <koos.gabriella92@gmail.com>
2024-06-21 11:47:05 +0200

    fetch meals

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from "react";
+
 import Header from "./components/Header";
+import { fetchMeals } from "./http";
 
 function App() {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+  const [mealsData, setMealsData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchMeals();
+        setMealsData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch meals" });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header />
+      <div id="meals">
+        {mealsData.map((meal) => (
+          <p key={meal.id}>{meal.name}</p>
+        ))}
+      </div>
     </>
   );
 }
